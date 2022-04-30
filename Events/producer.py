b0255ce4b7fcafde8dcfeb4097988c25fd91e6f2
Mahamat Naim Abdelkerim <73735806+NaimCode@users.noreply.github.com>
2022-04-30 05:36:14 +0000

    setting up env

---
+++ Events/producer.py
@@ -0,0 +1,21 @@
+from kafka import KafkaProducer
+import time
+import json
+from random import seed
+from random import randint
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092',
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+topic="price";
+seed(1)
+for i in range(0, 1000):
+    print(str(i))
+    producer.send(topic,[
+        {"product": "Mouse", "price": randint(0, 200)},
+        {"product": "Keyboard", "price": randint(20, 300)},
+        {"product": "Monitor", "price": randint(100, 600)},
+        {"product": "USB 32GB", "price": randint(0, 150)},
+        {"product": "HDMI", "price": randint(0, 50)},
+        {"product": "PC", "price": randint(200, 600)},
+    ] )
+    time.sleep(5)
---
+++ Server/index.js
@@ -0,0 +1,45 @@
+const { Kafka } = require("kafkajs");
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: { origin: "*" },
+  path: "/chart",
+});
+const kafka = new Kafka({
+  clientId: "Server Node",
+  brokers: ["localhost:9092"],
+});
+
+io.on("connection", async (socket) => {
+  console.log("connection");
+
+  const consumer = kafka.consumer({ groupId: "client" });
+  await consumer.connect();
+  await consumer.subscribe({ topic: "price", fromBeginning: false });
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      socket.emit("price", {
+        data: JSON.parse(message.value.toString()),
+        date: new Date().toLocaleTimeString(),
+      });
+    },
+  });
+  socket.on("disconnect", () => {
+    console.log("client disconnected");
+  });
+});
+
+server.listen(4000, () => console.log("Server is running"));
+
+app.get("/ws", async (req, res) => {
+  const consumer = kafka.consumer({ groupId: "client" });
+
+  await consumer.connect();
+  await consumer.subscribe({ topic: "price", fromBeginning: false });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log(JSON.parse(message.value.toString()));
+    },
+  });
+});
